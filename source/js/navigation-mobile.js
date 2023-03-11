4271541deaa4c38efd09e5e4ab8616068e73152d
CendresChaudes <romqaaa1337@gmail.com>
2023-03-11 19:25:05 +0300

    prepare for GitHub Pages

--- source/js/navigation-mobile.js
+++
@@ -1,5 +0,0 @@
-const headerNav = document.querySelector(".header__nav");
-const headerNavToggle = document.querySelector(".header__nav-toggle");
-
-headerNav.classList.remove("no-js");
-headerNavToggle.addEventListener("click", () => headerNav.classList.toggle("is-open"));
